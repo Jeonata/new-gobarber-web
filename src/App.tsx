f263b487fb46abd958b55574e05e8653caec4c4b
Jeonata Oliveira <jeo.nata@gmail.com>
2020-05-03 22:41:16 -0300

    chore: add and structure signIn/signUp pages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
